1576876951ec4c893c15a98a09781867fe4b00f3
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-08-28 21:52:28 +0100

    added reactnative contact

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 
 import React, {Component} from 'react';
 import { View,Text, StyleSheet, Image, Button} from 'react-native';
-import ImagePicker from "./src/ImagePicker"
+//import ImagePicker from "./src/ImagePicker"
 
 export default class App extends Component {
  
--- android/app/src/main/java/com/reactna/MainApplication.java
+++ android/app/src/main/java/com/reactna/MainApplication.java
@@ -11,6 +11,7 @@ import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 
 import java.util.List;
 
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
+      //packages.add( new ReactNativeContacts());
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       return packages;
