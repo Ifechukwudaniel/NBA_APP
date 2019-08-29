700a11f130caaf7b44e7193f60acc81e8146427e
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-08-29 15:14:45 +0100

    back to begining

--- App.js
+++ App.js
@@ -6,17 +6,10 @@ import DeviceInfo from "react-native-device-info"
 import Dimension from './src/Dimension';
 export default class App extends Component {     
   render() {    
-   DeviceInfo.getBatteryLevel()
-   .then(batteryLevel=>{
-      console.warn(batteryLevel)
-   })
-
-   console.warn(DeviceInfo.isLandscape());
     return (
       <View style={styles.container}>
         <Text> Am Working ooo</Text>
-      </View>  
-       
+      </View>   
     )
   }
 }
