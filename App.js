dfc0dd1ca15438c7a06ae44c8b719b1c92a37798
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-08-29 03:03:57 +0100

    contact us

--- App.js
+++ App.js
@@ -1,17 +1,69 @@
 
 
 import React, {Component} from 'react';
-import { View,Text, StyleSheet, Image, Button} from 'react-native';
-//import ImagePicker from "./src/ImagePicker"
+import { View,Text, StyleSheet, Image, Button, Platform, PermissionsAndroid     } from 'react-native';
+import Contacts from "react-native-contacts"
+
 
 export default class App extends Component {
- 
+  state = {
+    contacts:[]
+  }
+  async requestContactPermission(){
+    if (Platform.OS ==="ios"  ) {
+        return true
+    } else {
+       const granted= await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+          PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS
+        ]);
+        if (
+          (granted['android.permission.READ_CONTACTS']===PermissionsAndroid.RESULTS.GRANTED)
+         && (granted['android.permission.WRITE_CONTACTS']===PermissionsAndroid.RESULTS.GRANTED)
+         )
+          {
+           return  true
+        } else {
+           return false
+        }
+    }
+  }
+
+  getContact=()=>{
+    this.requestContactPermission()
+  .then((response)=>{
+    //  if (response===true) {
+    //      Contacts.getAll((err, contacts)=>{
+    //        if(err)
+    //         console.warn(err)
+    //       this.setState({contacts})
+    //      })
+    //  } else {
+    //     alert("no permission")
+    //  }
+      
+  })
+  }
+
   render() {
     return (
-
-         <ImagePicker/>
+      <View style={styles.container}>
+      {this.state.contacts.map((item ,i)=>(
+        <Text key={i}> {item.givenName}  </Text>
+      ))}
+        <Button title="Load Contacts" onPress={this.getContact}/>
+      </View>
     )
   }
 }
 
 
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#fffff5',
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
