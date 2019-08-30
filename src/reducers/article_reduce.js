4a9570fbd4a0ec4719d5227be5a61d44fc54396a
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-08-30 15:32:12 +0100

    remove unnessary file

--- src/reducers/article_reduce.js
+++
@@ -1,8 +0,0 @@
-export default function (state={}, action) {
-      switch (action.type) {
-          case "GET_ARTICLE":
-                 return{ ...state, posts:action.payload}
-          default:
-                return state
-      }
-}
