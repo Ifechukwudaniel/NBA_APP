65a3156ba5c6d42e926d8a11157384a0cffe94f6
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-08-30 21:47:50 +0100

    redux setup

--- app/App.js
+++ app/App.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import { View,Text, StyleSheet, Animated} from 'react-native';
 // import AnimOne  from "./src/AnimOne"
 // import AnimTwo from "./src/AnimTwo"
-import AnimThree from "./src/AnimThree"
+import AnimThree from "../src/AnimThree"
 export default class App extends Component {     
   render() {    
     return (
