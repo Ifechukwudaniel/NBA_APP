b0260091ae5d9a4dc5ccae767c2b8ca8887df16a
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-08-31 00:08:07 +0100

    basic  navigation

--- app/components/Games/index.js
+++ app/components/Games/index.js
@@ -4,7 +4,6 @@ import React, {Component} from 'react';
 import { View,Text, StyleSheet,} from 'react-native';
 export default class GamesComponent extends Component {     
   render() {    
-     const Nav = RootNavigator()
     return (
       <View>
         <Text> The Games Component</Text>
--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,33 @@ import {
     createAppContainer, 
     createStackNavigator, 
     createBottomTabNavigator,
-    createSwitchNavigator
+    createSwitchNavigator,
+    createMaterialTopTabNavigator
 } from "react-navigation"
 import { Platform } from 'react-native';
+
+//screen
+
+import SignIn from "./components/Auth"
+import Games from "./components/Games"
+import News from "./components/News"
+
+
+const AppStack =  createBottomTabNavigator({
+    Games:Games,
+    News:News
+})
+
+const AuthStack = createStackNavigator({
+   SignIn:SignIn
+})
+
+
+export const  RootNavigator= ()=>{
+     return createAppContainer(createSwitchNavigator({
+         App:AppStack,
+         Auth:AuthStack
+     },{
+         initialRouteName:'Auth'
+     }))
+}
