6c36bec06d9f07f1b10f9e2aafa68063978a0d08
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-08-30 15:27:49 +0100

    redux with  react native

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
-
+import React from "react"
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Provider} from "react-redux";
+import  {createStore, applyMiddleware} from "redux";
+import promiseMiddleware from 'redux-promise';
+import reducer from "./src/reducers"
 
-AppRegistry.registerComponent(appName, () => App);
+const createStoreWithMiddleware=applyMiddleware(promiseMiddleware)(createStore)
+
+const appRedux= ()=>(
+    <Provider store={createStoreWithMiddleware(reducer)}>
+        <App/>
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => appRedux);
