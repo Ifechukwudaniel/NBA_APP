9e8b2bbbe5a8e93b4846e80da37eaaa95f0eb7e4
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-09-03 23:43:06 +0100

    had firebase issue so had to simulate datebase

--- app/components/News/index.js
+++ app/components/News/index.js
@@ -15,7 +15,21 @@ import Preloader from '../../../utils/Preloder';
     news.stories?
       news.stories.map(story=>{
          return(
-           <Text key={story.id}>hdwhdwudhwd</Text>
+          <TouchableOpacity key={story.id} style={styles.cardContainer}> 
+            <View>
+              <Image
+               style={{height:150, justifyContent:"space-around"}} 
+               source={{uri:`${story.image}`}} 
+                resizeMode="cover"
+               />
+            </View>
+            <View style={styles.contentCard}>
+              <Text style={styles.titleCard}> {story.title}</Text>
+            </View>
+            <View style={styles.bottomCard}>
+              <Text style={styles.team}> {story.team}</Text>
+            </View>
+          </TouchableOpacity>
          )
       })
     : <Preloader/>
@@ -31,10 +45,34 @@ import Preloader from '../../../utils/Preloder';
 
 
 const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: '#fff',
-  }
+  cardContainer:{
+    backgroundColor:"#fff",
+    margin:10,
+    shadowColor:"#dddddd",
+    shadowOffset:{width:0, height:3},
+    shadowOpacity:0.8,
+    shadowRadius:2,
+    elevation:1,
+    borderRadius:2
+  },
+  contentCard:{
+    borderWidth:1,
+    borderColor:"#dddddd"
+  },
+  titleCard:{
+    color:"#232323",
+    fontSize:16,
+    padding:10
+  },
+  bottomCard:{
+    color:"#232323",
+    fontSize:5,
+    padding:5
+  },
+  team:{
+    color:"#231233",
+  },
+  
 });
 
 
--- app/store/actions/news.actions.js
+++ app/store/actions/news.actions.js
@@ -1,24 +1,33 @@
 import { GET_ALL_NEWS } from "../types";
 import axios from "axios";
 import { DB_URL } from "../../../utils/config";
+import data from "../../database/data"
 
 
 export  const  getAllNews= ()=>{
-    const request =  axios.get(`${DB_URL}/news.json`)
-        .then(response=>{
-                let newArray=[]
-                for (const key in response.data) {
-                newArray.push({
-                        id:key,
-                        ...response.data[key]
-                })
-                }
-                return newArray;
-            })
-            .catch(()=>{
-                return false
-        })
-
+    // const request =  axios.get(`${DB_URL}/news.json`)
+    //     .then(response=>{
+    //             let newArray=[]
+    //             for (const key in response.data) {
+    //             newArray.push({
+    //                     id:key,
+    //                     ...response.data[key]
+    //             })
+    //             }
+    //             console.log(newArray)
+    //             return newArray;
+    //         })
+    //         .catch(e=>{
+    //             console.warn(e)
+    //             return false
+    //     })
+     const request =[] 
+         for (const key in data.news) {
+           request.push({
+               id:key,
+              ...data.news[key]
+           })
+        }
     return {
         type:GET_ALL_NEWS,
         payload:request
