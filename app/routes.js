62cdd052b232f47ccbeea3ff50064f909be1faca
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-09-03 14:33:32 +0100

    fisished logo images

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,6 @@ import {
     createMaterialTopTabNavigator,
 } from "react-navigation"
 import { Platform } from 'react-native';
-import  Icon from 'react-native-vector-icons'
 
 //screen
 
@@ -17,6 +16,10 @@ import GamesArticle from "./components/Games/GameArticle"
 import News from "./components/News"
 import NewArticle from "./components/News/NewArticle"
 import Logo from "../utils/Logo"
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const myIcon1 = <Icon name="ios-basketball" size={30} color="#fff" />; // Defaults to regular
+const myIcon2 = <Icon name="md-tv"  size={30} color="#fff" solid />;
 
 const headerConf = {
     headerLayoutPreset:"center",
@@ -26,6 +29,7 @@ const headerConf = {
         },
         headerTitle:Logo,
         headerTintColor:"white"
+
     }
 }
 
@@ -45,15 +49,27 @@ const AppStack =  createBottomTabNavigator({
     News:{
       screen:NewsStack,
       navigationOptions:{
-
+        tabBarIcon:myIcon1
       }
     },
     Games:{
         screen:GamesStack,
         navigationOptions:{
-            tabBarIcon:(<t></t>)
+          tabBarIcon:myIcon2,
+          title:"Games"
         }
-      }
+      },
+},{
+    tabBarOptions:{
+        activeTintColor:"#fff",
+        showLabel:false,
+        activeBackgroundColor:"#00194b",
+        inactiveBackgroundColor:"#001338",
+        style:{
+            color:"white",
+            backgroundColor:"#001338"
+        }
+    }
 })
 
 const AuthStack = createStackNavigator({
