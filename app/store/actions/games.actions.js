8dcaa1a48b9d4dfffd719be8dc26caa754c568f2
CodesDaniel <mailidanielifechukwu@gmail.com>
2019-09-04 22:36:40 +0100

    get data for teams and games from firebase

--- app/store/actions/games.actions.js
+++ app/store/actions/games.actions.js
@@ -1,7 +1,19 @@
 import { GET_ALL_GAMES } from "../types";
+import Axios from "axios";
+import {DB_URL} from "../../../utils/config"
+import { arrangeForm } from "../../components/Auth/formUtil";
 
 export const getAllGames=()=>{
-     
+  const  request = new Promise((resolve, reject)=>{
+        Axios.get(`${DB_URL}/teams.json`)
+        .then(response=>{
+            const teams = arrangeForm(response.data)
+            Axios.get(`${DB_URL}/games.json`)
+            .then( games=>{
+                  const games= arrangeForm(result.data)
+            })
+        })
+  })
    return{
        type:GET_ALL_GAMES,
        payload:{
